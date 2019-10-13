fb5089d5d4390bc1543a6a81ce64ec77689d13f0
Frédérick Capovilla <caposite@hotmail.com>
2019-10-12 20:55:55 -0400

    Ajout de SCSS, bootstrap et fontawesome.

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -4,6 +4,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = (env, options) => ({
   optimization: {
@@ -31,11 +32,17 @@ module.exports = (env, options) => ({
       {
         test: /\.css$/,
         use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
+      },
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      },
+      { test: /\.(png|woff|woff2|eot|ttf|svg)$/, loader: 'url-loader?limit=100000' }
     ]
   },
   plugins: [
     new MiniCssExtractPlugin({ filename: '../css/app.css' }),
-    new CopyWebpackPlugin([{ from: 'static/', to: '../' }])
+    new CopyWebpackPlugin([{ from: 'static/', to: '../' }]),
+    new webpack.ProvidePlugin({jQuery: 'jquery', $: 'jquery'})
   ]
 });
--- lib/agregat_web/live/folder_live.ex
+++ lib/agregat_web/live/folder_live.ex
@@ -10,10 +10,10 @@ defmodule AgregatWeb.FolderLive do
 
   def mount(_session, socket) do
     folders = Feeds.list_folders()
-    {:ok, assign(socket, folders: folders)}
+    {:ok, assign(socket, folders: folders, selected: nil, total_unread: 0, menu_open: nil)}
   end
 
-  def handle_event("toggle" <> folder_id, _, socket) do
+  def handle_event("toggle-folder-" <> folder_id, _, socket) do
     folder_id = String.to_integer(folder_id)
     folder = Enum.find(socket.assigns.folders, &(&1.id == folder_id))
     folder_params = if folder.open do
