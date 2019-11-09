ffe2ce8314fab81e20f35173df8c83473cc0c881
Frédérick Capovilla <caposite@hotmail.com>
2019-11-08 23:42:21 -0500

    Delete compilation and dependency folders when building image.

--- release/push.sh
+++ release/push.sh
@@ -5,5 +5,6 @@ set -e
 
 cd ..
 echo "REMEMBER: Login using 'docker login docker.pkg.github.com -u USERNAME -p TOKEN'"
+rm -rf deps _build assets/node_modules
 docker build -t docker.pkg.github.com/fcapovilla/agregat/agregat:${1:-latest} -f release/Dockerfile .
 docker push docker.pkg.github.com/fcapovilla/agregat/agregat:${1:-latest}
