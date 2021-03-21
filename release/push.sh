fde0bc9789c7d45e7e1e2d94a369fc2bc54b3abb
Frédérick Capovilla <caposite@hotmail.com>
2021-03-20 20:31:03 -0400

    Use ghcr package repository.

--- release/push.sh
+++ release/push.sh
@@ -4,7 +4,7 @@ cd "$(dirname "$0")"
 set -e
 
 cd ..
-echo "REMEMBER: Login using 'docker login docker.pkg.github.com -u USERNAME -p TOKEN'"
+echo "REMEMBER: Login using 'docker login ghcr.io -u USERNAME -p TOKEN'"
 rm -rf deps _build assets/node_modules
-docker build -t docker.pkg.github.com/fcapovilla/agregat/agregat:${1:-latest} -f release/Dockerfile .
-docker push docker.pkg.github.com/fcapovilla/agregat/agregat:${1:-latest}
+docker build -t ghcr.io/fcapovilla/agregat:${1:-latest} -f release/Dockerfile .
+docker push ghcr.io/fcapovilla/agregat:${1:-latest}
